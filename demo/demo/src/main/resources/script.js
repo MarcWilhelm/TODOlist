7402e9a6707e14a74e0e8542e0050930ca22a091
Marc Wilhelm <73398298+MarcWilhelm@users.noreply.github.com>
2022-06-02 16:21:36 +0200

    Start 2

--- demo/demo/src/main/resources/script.js
+++ demo/demo/src/main/resources/script.js
@@ -1,1 +1,6 @@
 var test = document.getElementById("test");
+test.innerHTML = "Hello World";
+let button = document.createElement("button");
+button.setAttribute("id","buttoncreate");
+button.innerHTML = "Create";
+test.appendChild(button);
