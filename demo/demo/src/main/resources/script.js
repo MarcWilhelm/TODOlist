7dab8344cfe4c5be23c9499681425aecf7a0ac5b
Marc Wilhelm <73398298+MarcWilhelm@users.noreply.github.com>
2022-06-09 16:36:27 +0200

    Start 2

--- demo/demo/src/main/resources/script.js
+++ demo/demo/src/main/resources/script.js
@@ -1,6 +1,31 @@
 var test = document.getElementById("test");
-test.innerHTML = "Hello World";
-let button = document.createElement("button");
-button.setAttribute("id","buttoncreate");
-button.innerHTML = "Create";
-test.appendChild(button);
+var forms = document.getElementById("form");
+let x = 0;
+test.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const TextField = document.querySelector("form input").value;
+    let divis = document.getElementById("todos")
+    let todo = document.createElement("p")
+    todo.setAttribute("id","x"+x)
+    todo.innerHTML = TextField;
+
+    if (x == 0){
+        divis.appendChild(todo);}
+    else{
+        divis.prepend(todo);
+    }
+    x++;
+    todo.onclick = löschen;
+    function löschen(){
+        let buttonid = event.target.id;
+        console.log(buttonid);
+        let deleze = document.getElementById(buttonid)
+        console.log(deleze);
+        deleze.remove();
+    }
+
+
+
+    });
+
+
